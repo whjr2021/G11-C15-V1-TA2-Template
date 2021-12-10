380395f7ffbfa6d921afb8581f347cc5d71e1259
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-12-10 14:28:36 +0530

    Add files via upload

---
+++ C15_TA2_Template.py
@@ -0,0 +1,8 @@
+# From "firebase" library import "firebase" module
+
+
+# Establish connection between Python and Car Racing databse created on Firbase.
+# The connection is stored in the variable "db"
+
+
+# Add player1 data to db
